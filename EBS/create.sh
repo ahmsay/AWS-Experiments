635913ffb091d9b142d72f810d0dd54c3d4abd79
ahmsay <ahmsay@gmail.com>
2022-12-11 12:27:18 +0300

    ebs example is removed

--- EBS/create.sh
+++
@@ -1,4 +0,0 @@
-# --size is GB
-aws ec2 create-volume --volume-type gp2 --size 10 --availability-zone <your_az>
-# for attaching and mounting, see AWS docs
-# volume and the instance must be in the same AZ
